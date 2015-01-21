8fb8efa5c5334b7a7b7123c78fec98d3e5eedad2
daveglouie <fairtexfan@gmail.com>
2015-01-20 23:02:28 -0800

    another change

--- web/index.php
+++ web/index.php
@@ -14,7 +14,7 @@ $app->register(new Silex\Provider\MonologServiceProvider(), array(
 
 $app->get('/', function() use($app) {
   $app['monolog']->addDebug('logging output.');
-  return 'Hello';
+  return 'Hello?  Is it me you\'re looking for? Really?';
 });
 
 $app->run();
